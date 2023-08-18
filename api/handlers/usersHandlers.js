328f3733506c0b288e04a8ac09b23cf3c8d40a6f
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-18 16:07:44 -0300

    Handler de prueba para ruta get de usuarios

--- api/handlers/usersHandlers.js
+++ api/handlers/usersHandlers.js
@@ -2,7 +2,10 @@ const getAllUsersController = require('../controllers/users');
 
 const getAllUsers = async (req, res) => {
   try {
+    const allUsers = getAllUsersController();
   } catch (error) {
     res.status(500).send(error.message);
   }
 };
+
+module.exports = getAllUsers;
