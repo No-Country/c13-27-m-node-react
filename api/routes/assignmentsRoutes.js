fdfdcfe67964718394bb1cd2f0dfcf956a0200c8
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-08-21 19:52:52 -0300

    Agregada ruta de busqueda de materia por id

--- api/routes/assignmentsRoutes.js
+++ api/routes/assignmentsRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // Importo los handlers de materias
-const getAllAssignmentsHandler = require('../handlers/assignmentsHandlers');
+const {
+  getAllAssignmentsHandler,
+  getAssignmentByIdHandler,
+} = require('../handlers/assignmentsHandlers');
 
 router.get('/allAssignments', getAllAssignmentsHandler); // Ruta para obtener todos las materias (ver como integrar con alumnos y profesores)
+router.get('/:id', getAssignmentByIdHandler); // Ruta para obtener una materia usando el ID
 
 module.exports = router;
