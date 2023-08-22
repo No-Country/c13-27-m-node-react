cf700bb104dadb2fbefb3b09041198a389fa9f5c
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-22 09:38:42 -0300

    Middleware y ruta para registro de profesores

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,10 @@ const app = express();
 
 // Importo rutas de usuarios
 const usersRoutes = require('./usersRoutes');
+const teachersRoutes = require('./teachersRoutes');
 
 // Middlewares
 app.use('/users', usersRoutes);
+app.use('teachers', teacherRoutes);
 
 module.exports = app;
