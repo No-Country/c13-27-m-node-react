ececfe45cacf6071b0ffb389f892a6ac3e9b43f5
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-22 11:48:04 -0300

    Solución a errores de cors y líneas duplicadas

--- api/routes/studentsRoutes.js
+++ api/routes/studentsRoutes.js
@@ -5,18 +5,12 @@ const router = express.Router();
 const {
   getAllStudentsHandler,
   studentLoginHandler,
+  registerStudentHandler,
 } = require('../handlers/studentsHandlers');
 
 router.get('/allStudents', getAllStudentsHandler); // Ruta para obtener todos los usuarios (actualmente es de prueba)
 router.get('/studentsLogin', studentLoginHandler);
-// Importo los handlers de usuarios
-const {
-  getAllStudentsHandler,
-  registerStudentHandler,
-} = require('../handlers/studentsHandlers'); // Con este no funciona
-// const getAllUsersController = require('../controllers/users');
 
-router.get('/allStudents', getAllStudentsHandler); // Ruta para obtener todos los usuarios (actualmente es de prueba)
 router.post('/registerStudent', registerStudentHandler); // Ruta para registrar un estudiante
 
 module.exports = router;
