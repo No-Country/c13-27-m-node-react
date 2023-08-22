c794f9f3d1e7289531f3c3d3edaad99e0671fead
Santiago Aquino <santiagoaquino01@hotmail.com>
2023-08-22 10:58:10 -0300

    Agregando ruta del registro de estudiantes

--- api/handlers/studentsHandlers.js
+++ api/handlers/studentsHandlers.js
@@ -1,12 +1,29 @@
-const getAllStudentsController = require('../controllers/studentsController');
+const {
+  getAllStudentsController,
+  registerStudentController,
+} = require('../controllers/studentsController');
 
 const getAllStudentsHandler = async (req, res) => {
   try {
-    const allStudents = await getAllStudentsController();
+    const allStudents = await getAllStudentsController(req, res);
     res.send(allStudents);
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
 
-module.exports = getAllStudentsHandler;
+const registerStudentHandler = async (req, res) => {
+  try {
+    const response = await registerStudentController(req, res);
+    //valido que el estudiante se haya guardado correctamente en la DB
+    if (!response) throw new Error('No se pudo registrar el usuario');
+    res.send('Usted se registró correctamente');
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+module.exports = {
+  getAllStudentsHandler,
+  registerStudentHandler,
+};
