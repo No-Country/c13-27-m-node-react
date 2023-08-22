e8be80af185d191d05895c6ceb33ec16d14bda0f
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-08-22 14:46:10 -0300

    frontedn -fuentes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import variables from '../styles/variables.module.scss';
+import { Inter } from 'next/font/google';
+
+export const metadata = {
+  title: 'Plataforma Universitaria',
+};
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 const page = () => {
   return <h1 style={{ color: variables.primaryColor }}>El futuro es hoy</h1>;
