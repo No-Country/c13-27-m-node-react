154e241653cc94a4f51a3c2492e8b104c5f7b532
aron0ne <arondiaz.cpn@gmail.com>
2023-08-22 19:55:25 -0300

    Agregué estructura y estilos al formulario de login

--- client/components/FormLogin.tsx
+++ client/components/FormLogin.tsx
@@ -1,5 +1,8 @@
 'use client';
-import { useState } from 'react';
+import Signup from '../app/signup/page';
+// import { useState } from 'react';
+import styles from '../styles/formlogin.module.scss';
+import Link from 'next/link';
 
 interface UserLogin {
   dni: string;
@@ -12,41 +15,59 @@ const initialForm: UserLogin = {
 };
 
 const FormLogin = () => {
-  const [loginForm, setLoginForm] = useState<UserLogin>(initialForm);
-
-  const handleForm = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setLoginForm({
-      ...loginForm,
-      [event.target.name]: event.target.value,
-    });
-  };
+  // const [loginForm, setLoginForm] = useState<UserLogin>(initialForm);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    //TODO VALIDACIONES
-    setLoginForm(loginForm);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="dni">DNI</label>
-      <input
-        type="text"
-        name="dni"
-        onChange={handleForm}
-      />
-      <label htmlFor="password">Contraseña</label>
-      <input
-        type="password"
-        name="password"
-        onChange={handleForm}
-      />
-      <input
-        type="submit"
-        value="Ingresar"
-      />
-    </form>
+    <div className={styles.container}>
+      <div className={styles.formcontainer}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputbox}>
+            <label htmlFor="dni" className={styles.label}>
+              Número de documento
+            </label>
+            <input
+              className={styles.input}
+              type="text"
+              name="dni"
+              placeholder="Ingrese su DNI"
+            />
+          </div>
+
+          <div className={styles.inputbox}>
+            <label htmlFor="password" className={styles.label}>
+              Contraseña
+            </label>
+            <input
+              className={styles.input}
+              type="password"
+              name="password"
+              placeholder="Ingrese su contraseña"
+            />
+          </div>
+
+          <div className={styles.forgotpassword}>
+            <a href="" className={styles.forgottext}>
+              Olvidé mi contraseña
+            </a>
+          </div>
+
+          <div className={styles.registerlink}>
+            No tienes cuenta?
+            <Link href="/signup">
+              <span> Regístrate! </span>
+            </Link>
+          </div>
+
+          <div className={styles.btncontainer}>
+            <input type="submit" value="Ingresar" className={styles.btn} />
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
