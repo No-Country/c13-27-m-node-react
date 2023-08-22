d1b85c124feb9cd313cf79034d07e1f5ce99fbe2
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-22 11:13:32 -0300

    Handler de login de estudiante

--- api/handlers/studentsHandlers.js
+++ api/handlers/studentsHandlers.js
@@ -1,4 +1,7 @@
-const getAllStudentsController = require('../controllers/studentsController');
+const {
+  getAllStudentsController,
+  studentLoginController
+} = require('../controllers/studentsController');
 
 const getAllStudentsHandler = async (req, res) => {
   try {
@@ -9,4 +12,18 @@ const getAllStudentsHandler = async (req, res) => {
   }
 };
 
-module.exports = getAllStudentsHandler;
+
+const studentLoginHandler = async (req, res) => {
+  const {email, password, check} = req.body;
+  try {
+    const login = await studentLoginController(email, password, check);
+    res.send(login);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+};
+
+module.exports = {
+  getAllStudentsHandler,
+  studentLoginHandler
+};
