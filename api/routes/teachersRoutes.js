1a6cc7996a44b34b4fb7e18789805b688417a32b
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-22 09:53:11 -0300

    Import de teachersHandler fixed

--- api/routes/teachersRoutes.js
+++ api/routes/teachersRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const teacherRegistryHandler = require('../handlers/teachersHandler');
+const {teacherRegistryHandler} = require('../handlers/teachersHandler');
 
 router.post('registry', teacherRegistryHandler);
 
