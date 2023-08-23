ae8859199044687b21c77d3d526992e6932770df
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-08-23 11:37:36 -0300

    renombrado todo 'AssignmentsModel' a'AssignmentModel' en controlador

--- api/controllers/assignmentsController.js
+++ api/controllers/assignmentsController.js
@@ -1,14 +1,14 @@
-const AssignmentsModel = require('../models/assignmentsModel'); // Llama al modelo AssignmentModel
+const AssignmentModel = require('../models/assignmentModel'); // Llama al modelo AssignmentModel
 
 const getAllAssignmentsController = async (req, res) => {
-  const assignments = await AssignmentsModel.find({}); // Todos las materias de la DB
+  const assignments = await AssignmentModel.find({}); // Todos las materias de la DB
   if (!assignments) throw new Error('No se pudieron obtener las materias');
   return assignments;
 };
 
 const getAssignmentByIdController = async (req, res) => {
   const { id } = req.params;
-  const assignment = await AssignmentsModel.findById(id).populate('students');
+  const assignment = await AssignmentModel.findById(id).populate('students');
   if (!assignment) throw new Error('No se pudo encontrar la materia');
   return assignment;
 };
