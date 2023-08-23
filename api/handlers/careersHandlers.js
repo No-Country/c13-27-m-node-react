c59d983aa5b4c1889cee113cd9717c8db89f6891
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-08-23 11:42:23 -0300

    renombrados controladores de carrera

--- api/handlers/careersHandlers.js
+++ api/handlers/careersHandlers.js
@@ -1,33 +1,32 @@
 const {
-    getAll,
-    getById,
-  } = require('../controllers/careersController');
-  
-  const getAllCareersHandler = async (req, res) => {
-    try {
-      const allCareers = await getAll();
-      if(!allCareers) {
-        return res.status(404).json({ error: 'No hay información disponible' });
-      };
+  getAllCareersController,
+  getCareerByIdController,
+} = require('../controllers/careersController');
 
-      res.json(allCareers);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+const getAllCareersHandler = async (req, res) => {
+  try {
+    const allCareers = await getAllCareersController();
+    if (!allCareers) {
+      return res.status(404).json({ error: 'No hay información disponible' });
     }
-  };
-  
-  const getCareersByIdHandler = async (req, res) => {
-    try {
-      const career = await getById(req);
-      if(!careers) {
-        return res.status(404).json({ error: 'No hay información disponible' });
-      };
 
-      res.json(career);
-    } catch (error) {
-      res.status(500).json({ error: 'Internal server error' });
+    res.json(allCareers);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+const getCareersByIdHandler = async (req, res) => {
+  try {
+    const career = await getCareerByIdController(req);
+    if (!careers) {
+      return res.status(404).json({ error: 'No hay información disponible' });
     }
-  };
-  
-  module.exports = { getAllCareersHandler, getCareersByIdHandler };
-  
+
+    res.json(career);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = { getAllCareersHandler, getCareersByIdHandler };
