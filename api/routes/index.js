62805f7d0944e3fe6b72c3adcf6c15b02bcab844
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-08-23 11:43:40 -0300

    agregado careersRoutes a index de rutas

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,10 +5,12 @@ const app = express();
 const studentsRoutes = require('./studentsRoutes');
 const teachersRoutes = require('./teachersRoutes');
 const assignmentsRoutes = require('./assignmentsRoutes');
+const careersRoutes = require('./careersRoutes');
 
 // Middlewares
 app.use('/students', studentsRoutes);
 app.use('/teachers', teachersRoutes);
 app.use('/assignments', assignmentsRoutes);
+app.use('/careers', careersRoutes);
 
 module.exports = app;
