8e045cfe2ec9ed6e41dc439f6cef8d8b291f7e0f
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-08-23 16:34:16 -0300

    Removidos ifs y try catch redundantes en controladores y handlers de carreras

--- api/controllers/careersController.js
+++ api/controllers/careersController.js
@@ -1,26 +1,16 @@
 const CareerModel = require('../models/careerModel'); // Llama al modelo CareerModel
 
 const getAllCareersController = async (req, res) => {
-  try {
-    const careers = await CareerModel.find({}); // Todos las carreras de la DB
-    if (!careers) throw new Error('No hay información disponible');
-
-    return careers;
-  } catch (error) {
-    res.status(404).json(error.message);
-  }
+  const careers = await CareerModel.find({}); // Todos las carreras de la DB
+  if (!careers) throw new Error('No hay información disponible');
+  return careers;
 };
 
 const getCareerByIdController = async (req, res) => {
   const { id } = req.params;
-  try {
-    const careers = await CareerModel.findById(id).populate('students');
-    if (!careers) throw new Error('No hay información disponible');
-
-    return careers;
-  } catch (error) {
-    res.status(404).json(error.message);
-  }
+  const careers = await CareerModel.findById(id).populate('students');
+  if (!careers) throw new Error('No hay información disponible');
+  return careers;
 };
 
 module.exports = {
