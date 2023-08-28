2441877de064b4fa3b9ab784b9e4188d4a261c1c
Saira Areco <sairaareco@gmail.com>
2023-08-28 14:35:44 -0300

    landing page

--- client/components/Landing.tsx
+++ client/components/Landing.tsx
@@ -9,14 +9,14 @@ const Landing = () => {
 
       <div className={styles.containerImages}>
         <div className={styles.imageGrid}>
-          <img src="../assets/noticias-eventos 2.png" alt="" />
-          <img src="../assets/noticias-eventos 1.png" alt="" />
-          <img src="../assets/noticias-eventos.png" alt="" />
+          <img src="../assets/noticias-eventos2.png" alt="" />
+          <img src="../assets/noticias-eventos1.png" alt="" />
+          <img src="../assets/noticias-eventos3.png" alt="" />
         </div>
         <div className={styles.imageGridBottom}>
-          {/* <img src="../assets/noticia 4.png" alt="" />
-          <img src="../assets/noticia 5.png" alt="" />
-          <img src="../assets/noticias 6.png" alt="" /> */}
+          <img src="../assets/noticias-eventos4.png" alt="" />
+          <img src="../assets/noticias-eventos5.png" alt="" />
+          <img src="../assets/noticias-eventos6.png" alt="" />
         </div>
       </div>
     </main>
