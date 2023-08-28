6f0ff5bd6bd70b9259cae71bd2ec96730585ea63
Saira Areco <sairaareco@gmail.com>
2023-08-28 14:43:27 -0300

    cambios en landing

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,11 +1,25 @@
 import styles from '../../styles/home.module.scss';
-import Landing from '../../components/Landing';
 
 const Home = () => {
   return (
-    <div>
-      <Landing />
-    </div>
+    <main>
+      <div className={styles.containerTitle}>
+        <h1 className={styles.title}> Eventos y Noticias </h1>
+      </div>
+
+      <div className={styles.containerImages}>
+        <div className={styles.imageGrid}>
+          <img src="../assets/noticias-eventos2.png" alt="" />
+          <img src="../assets/noticias-eventos1.png" alt="" />
+          <img src="../assets/noticias-eventos3.png" alt="" />
+        </div>
+        <div className={styles.imageGridBottom}>
+          <img src="../assets/noticias-eventos4.png" alt="" />
+          <img src="../assets/noticias-eventos5.png" alt="" />
+          <img src="../assets/noticias-eventos6.png" alt="" />
+        </div>
+      </div>
+    </main>
   );
 };
 
