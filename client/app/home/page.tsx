1b35aa0139d6c3116cd5565c8726799ffde1b80a
Saira Areco <sairaareco@gmail.com>
2023-08-28 10:25:08 -0300

    inicios de la landing page

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,9 +1,21 @@
-const page = () => {
+import styles from '../../styles/home.module.scss';
+
+const Home = () => {
   return (
     <main style={{ display: 'grid', placeItems: 'center' }}>
-      <h1>Aquí estará el Home de la plataforma</h1>;
+      <div className={styles.containerTitle}>
+        <h1 className={styles.title}> Eventos y Noticias </h1>
+      </div>
+      <div className={styles.imageGrid}>
+        <img src="../assets/noticias-eventos 2.png" alt="" />
+        <img src="../assets/noticias-eventos 1.png" alt="" />
+        <img src="../assets/noticias-eventos.png" alt="" />
+        <img src="../assets/noticias-eventos 3.png" alt="" />
+        <img src="../assets/noticias-eventos 4.png" alt="" />
+        <img src="../assets/noticias-eventos 5.png" alt="" />
+      </div>
     </main>
   );
 };
 
-export default page;
+export default Home;
