a967761e9e15901b71ad962ca5e1f28f4449fbef
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-29 12:07:29 -0300

    Esquema de validaciones para el login de usuarios

--- api/utils/validations/studentsValidations.js
+++ api/utils/validations/studentsValidations.js
@@ -1,8 +1,11 @@
 const joi = require('joi');
 
 const loginSchema = joi.object({
-  name: joi.string().required(),
-  email: joi.string().email().required(),
+  dni: joi
+    .string()
+    .pattern(new RegExp(/^[0-9]+$/i))
+    .min(5)
+    .required(),
   password: joi.string().required(),
 });
 
@@ -32,28 +35,6 @@ const registrySchema = joi
     password: joi.string().required(),
   })
   .with('password', 'repeat_password');
-
-// const Joi = require('joi');
-
-// const schema = Joi.object({
-//   username: Joi.string().alphanum().min(3).max(30).required(),
-
-//   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-
-//   repeat_password: Joi.ref('password'),
-
-//   access_token: [Joi.string(), Joi.number()],
-
-//   birth_year: Joi.number().integer().min(1900).max(2013),
-
-//   email: Joi.string().email({
-//     minDomainSegments: 2,
-//     tlds: { allow: ['com', 'net'] },
-//   }),
-// })
-//   .with('username', 'birth_year')
-//   .xor('password', 'access_token')
-//   .with('password', 'repeat_password');
 
 module.exports = {
   loginSchema,
