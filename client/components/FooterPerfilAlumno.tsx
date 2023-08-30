5ea80f0da42f62d09c10112a06d3d4c33affc143
Saira Areco <sairaareco@gmail.com>
2023-08-30 19:48:26 -0300

    creacion boton +

--- client/components/FooterPerfilAlumno.tsx
+++ client/components/FooterPerfilAlumno.tsx
@@ -1,51 +1,65 @@
-import { useEffect } from 'react';
 import styles from '../styles/footerperfilalumno.module.scss';
+import { useState } from 'react';
 
 const FooterPerfilAlumno = () => {
+  const [showEvents, setShowEvents] = useState(false);
+
+  const handleShowEvents = () => {
+    setShowEvents(!showEvents);
+  };
+
   return (
     <section>
-      <h1 className={styles.titleFooter}> EVENTOS ESPECIALES </h1>
-      <div className={styles.containerAllEvents}>
-        <div className={styles.boxContainer}>
-          <div className={styles.divisionDate}>
-            <p className={styles.date}> Miercoles </p>
-            <p className={styles.number}> 25 </p>
-          </div>
-          <div className={styles.divisionSubject}>
-            <p className={styles.subject}> Examen Biología </p>
-          </div>
-        </div>
-
-        <div className={styles.boxContainer}>
-          <div className={styles.divisionDate}>
-            <p className={styles.date}> Jueves </p>
-            <p className={styles.number}> 01 </p>
-          </div>
-          <div className={styles.divisionSubject}>
-            <p className={styles.subject}> Entrega TP Anato </p>
-          </div>
-        </div>
-
-        <div className={styles.boxContainer}>
-          <div className={styles.divisionDate}>
-            <p className={styles.date}> Miercoles </p>
-            <p className={styles.number}> 25 </p>
-          </div>
-          <div className={styles.divisionSubject}>
-            <p className={styles.subject}> Examen Biología </p>
-          </div>
-        </div>
-
-        <div className={styles.boxContainer}>
-          <div className={styles.divisionDate}>
-            <p className={styles.date}> Jueves </p>
-            <p className={styles.number}> 01 </p>
-          </div>
-          <div className={styles.divisionSubject}>
-            <p className={styles.subject}> Entrega TP Histo </p>
-          </div>
-        </div>
+      <div className={styles.containerBtnTitle}>
+        <button className={styles.btn} onClick={handleShowEvents}>
+          +
+        </button>
+        <h1 className={styles.titleFooter}> EVENTOS ESPECIALES </h1>
       </div>
+
+      {showEvents && (
+        <div className={styles.containerAllEvents}>
+          <div className={styles.boxContainer}>
+            <div className={styles.divisionDate}>
+              <p className={styles.date}> Miercoles </p>
+              <p className={styles.number}> 25 </p>
+            </div>
+            <div className={styles.divisionSubject}>
+              <p className={styles.subject}> Examen Biología </p>
+            </div>
+          </div>
+
+          <div className={styles.boxContainer}>
+            <div className={styles.divisionDate}>
+              <p className={styles.date}> Jueves </p>
+              <p className={styles.number}> 01 </p>
+            </div>
+            <div className={styles.divisionSubject}>
+              <p className={styles.subject}> Entrega TP Anato </p>
+            </div>
+          </div>
+
+          <div className={styles.boxContainer}>
+            <div className={styles.divisionDate}>
+              <p className={styles.date}> Miercoles </p>
+              <p className={styles.number}> 25 </p>
+            </div>
+            <div className={styles.divisionSubject}>
+              <p className={styles.subject}> Examen Biología </p>
+            </div>
+          </div>
+
+          <div className={styles.boxContainer}>
+            <div className={styles.divisionDate}>
+              <p className={styles.date}> Jueves </p>
+              <p className={styles.number}> 01 </p>
+            </div>
+            <div className={styles.divisionSubject}>
+              <p className={styles.subject}> Entrega TP Histo </p>
+            </div>
+          </div>
+        </div>
+      )}
 
       <section className={styles.containerBox}>
         <div className={styles.box}>
