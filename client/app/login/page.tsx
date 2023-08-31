eb282b98a4c5ca50a2cd108951d22979c1a5d7c6
aron0ne <arondiaz.cpn@gmail.com>
2023-08-31 19:43:57 -0300

    Actualicé los estilos del login

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,20 +1,11 @@
 import FormLogin from '../../components/FormLogin';
 import styles from '../../styles/formlogin.module.scss';
-import Image from 'next/image';
-import login from '../../public/assets/login.png';
 
 const Login = () => {
   return (
-    <main className={styles.mainlogin}>
+    <main>
       <FormLogin />
-      <aside className={styles.loginimagecontainer}>
-        <Image
-          className={styles.loginimage}
-          src={login}
-          width={500}
-          height={600}
-          alt="login"></Image>
-      </aside>
+      <aside className={styles.loginimagecontainer}></aside>
     </main>
   );
 };
