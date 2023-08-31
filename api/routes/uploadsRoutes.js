581a13fa6f5c390e5daccaef4432c52719f83216
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-08-31 19:34:29 -0300

    Ruta para subir pdf

---
+++ api/routes/uploadsRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+
+const app = express();
+
+// Configuración de Multer para el almacenamiento de archivos
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Carpeta donde se guardarán los archivos
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+// Ruta para subir archivos PDF
+app.post('/upload', upload.single('pdfFile'), (req, res) => {
+  res.json({ message: 'Archivo subido exitosamente' });
+});
