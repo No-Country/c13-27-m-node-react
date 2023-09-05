cb0ba3456e4e54d8129b431814863461de944592
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-09-05 11:01:16 -0300

    creaión de carpetas materias y tareas en perfil alumno y profesor

---
+++ client/app/perfil-alumno/tareas/page.tsx
@@ -0,0 +1,9 @@
+const TareasAlumno = () => {
+  return (
+    <main>
+      <h1>Tareas Alumno</h1>
+    </main>
+  );
+};
+
+export default TareasAlumno;
---
+++ client/app/perfil-profesor/materias/page.tsx
@@ -0,0 +1,9 @@
+const MateriasProfesor = () => {
+  return (
+    <main>
+      <h1>Materias Profesor</h1>
+    </main>
+  );
+};
+
+export default MateriasProfesor;
