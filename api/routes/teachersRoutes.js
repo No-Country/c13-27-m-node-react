679efe25ea70c39eaea7632c7a5996179938548c
Sergio Sarmiento <gersiomarsiento@gmail.com>
2023-09-05 15:05:15 -0300

    Creada ruta /assignmentsSelection/:id en teacherRoutes

--- api/routes/teachersRoutes.js
+++ api/routes/teachersRoutes.js
@@ -6,7 +6,8 @@ const {
   getAllTeachersHandler,
   teacherLoginHandler,
   registerTeacherHandler,
-  getTeacherByIdHandler
+  getTeacherByIdHandler,
+  teacherSelectionHandler,
 } = require('../handlers/teachersHandlers');
 
 router.get('/allTeachers', getAllTeachersHandler); // Ruta para obtener todos los usuarios
@@ -14,5 +15,6 @@ router.post('/teachersLogin', teacherLoginHandler);
 router.get('/:id', getTeacherByIdHandler);
 router.post('/registerTeacher', registerTeacherHandler); // Ruta a la que se envia el form de registro de profesor completado
 router.get('/teacher', getTeacherByIdHandler); //Ruta para obtener un profesor por id
+router.put('/assignmentsSelection/:id', teacherSelectionHandler); // Ruta para modificar materias del profesor
 
 module.exports = router;
