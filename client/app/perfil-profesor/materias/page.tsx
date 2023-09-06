28eaec7ad16465c3907f232d166f4cf17ed93d4e
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-09-06 16:49:13 -0300

    Disposición página de materias

--- client/app/perfil-profesor/materias/page.tsx
+++ client/app/perfil-profesor/materias/page.tsx
@@ -3,6 +3,7 @@ import { MateriaProfesor } from '../../../components/MateriaProfesor';
 import Image from 'next/image';
 import rectangle from '../../../public/assets/rectangle.png';
 import styles from '../../../styles/materialestudio.module.scss';
+import UserList from '../../../components/UserList';
 
 const MateriasProfesor = () => {
   return (
@@ -14,7 +15,14 @@ const MateriasProfesor = () => {
       />
       <main>
         <h1>NOMBRE MATERIA</h1>
-        <MateriaProfesor />
+        <div className={styles.grid}>
+          <div className={styles.gridLeft}>
+            <MateriaProfesor />
+          </div>
+          <div className={styles.gridRigth}>
+            <UserList />
+          </div>
+        </div>
       </main>
     </>
   );
