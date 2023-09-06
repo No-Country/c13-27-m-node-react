6b44b91f2c143efdeb2364acb5a359d2abd9f4c1
Saira Areco <sairaareco@gmail.com>
2023-09-06 17:16:49 -0300

    correcciones finales

--- client/app/perfil-alumno/tareas/page.tsx
+++ client/app/perfil-alumno/tareas/page.tsx
@@ -1,7 +1,15 @@
+'use client';
 import styles from '../../../styles/tareas.module.scss';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const TareasAlumno = () => {
+  const [readComent, setReadComent] = useState<boolean>(false);
+
+  const toggleComent = () => {
+    setReadComent(!readComent);
+  };
+
   return (
     <main>
       <h1 className={styles.title}> Tus trabajos </h1>
@@ -13,7 +21,22 @@ const TareasAlumno = () => {
           height={40}
           className={styles.pdfImage}
         />
-        
+        <div
+          className={`${styles.containerComent} ${
+            readComent ? styles.active : ''
+          }`}>
+          <p className={styles.coments} onClick={toggleComent}>
+            Comentarios
+          </p>
+        </div>
+        {readComent && (
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero,
+            deleniti. Quo quasi, perspiciatis voluptatum vel vitae sunt
+            praesentium ipsum, provident accusantium aut necessitatibus esse
+            voluptate impedit, minus exercitationem laborum? Itaque.
+          </p>
+        )}
       </div>
     </main>
   );
