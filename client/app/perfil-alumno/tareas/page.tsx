b31faeeefcae113b5e8a010bac648eec6f116a24
Aron Diaz <arondiaz.cpn@gmail.com>
2023-09-06 14:39:47 -0300

    Añadí componente para subir archivo pdf alumno

--- client/app/perfil-alumno/tareas/page.tsx
+++ client/app/perfil-alumno/tareas/page.tsx
@@ -1,7 +1,10 @@
+import { Subirpdf } from '../../../components/Subirpdf';
+
 const TareasAlumno = () => {
   return (
     <main>
       <h1>Tareas Alumno</h1>
+      <Subirpdf />
     </main>
   );
 };
