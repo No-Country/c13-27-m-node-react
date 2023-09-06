d7cc2418c5e34ca972d700832379d4830f15993e
Saira Areco <sairaareco@gmail.com>
2023-09-06 17:05:35 -0300

    eventos sigue estatico

--- client/app/perfil-alumno/tareas/page.tsx
+++ client/app/perfil-alumno/tareas/page.tsx
@@ -13,16 +13,7 @@ const TareasAlumno = () => {
           height={40}
           className={styles.pdfImage}
         />
-        <p className={styles.coments}> Comentarios </p>
-        <Image
-          src="/assets/pdf-icon.svg"
-          alt="icono de un doc pdf"
-          width={40}
-          height={40}
-          className={styles.pdfImage}
-        />
-        <p className={styles.coments}> Comentarios </p>
-        <div className={styles.containerComent}> </div>
+        
       </div>
     </main>
   );
--- client/app/perfil-profesor/tareas/page.tsx
+++ client/app/perfil-profesor/tareas/page.tsx
@@ -1,10 +1,15 @@
-// import { useState } from 'react';
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from '../../../styles/tareas.module.scss';
 
-
 const TareasProfesor = () => {
   // const [pdfContent, setPdfContent] = useState<string>('');
+  const [coment, setComent] = useState<boolean>(false);
+
+  const toggleComent = () => {
+    setComent(!coment);
+  };
 
   //   ASYNC AWAIT
   //   useEffect(() => {
@@ -33,9 +38,11 @@ const TareasProfesor = () => {
   // }, []);
 
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <h2 className={styles.title}> Trabajos entregados </h2>
       <div className={styles.containerPfd}>
+
+        {/* Primer pdf */}
         <Image
           src="/assets/pdf-icon.svg"
           alt="icono de un doc pdf"
@@ -43,44 +50,37 @@ const TareasProfesor = () => {
           height={40}
           className={styles.pdfImage}
         />
-        <p className={styles.coments}> Comentarios </p>
-        <Image
-          src="/assets/pdf-icon.svg"
-          alt="icono de un doc pdf"
-          width={40}
-          height={40}
-          className={styles.pdfImage}
-        />
-        <p className={styles.coments}> Comentarios </p>
-        <Image
-          src="/assets/pdf-icon.svg"
-          alt="icono de un doc pdf"
-          width={40}
-          height={40}
-          className={styles.pdfImage}
-        />
-        <p className={styles.coments}> Comentarios </p>
+        <div
+          className={`${styles.containerComent} ${
+            coment ? styles.active : ''
+          }`}>
+          <p className={styles.coments} onClick={toggleComent}>
+            Comentarios
+          </p>
+        </div>
+
+        {coment && (
+          <div className={styles.writeAndSendComent}>
+            <textarea placeholder="" className={styles.inputComent}></textarea>
+            <button className={styles.sendComent}> ENVIAR </button>
+          </div>
+        )}
+
       </div>
 
       {/* <iframe src={pdfContent} frameborder="0"></iframe> */}
-    </div>
+    </main>
   );
 };
 
 export default TareasProfesor;
 
 // post upload id para cargarr material desde el Alumno
-// 
+//
 //   GET: /upload (pasar assignment por body) ===> devuelve las clases en pdf de esa materia
 
-// tare alumno... 
+// tare alumno...
 // GET: /upload/:id (pasar assignment por body) ===> Devuelve las entregas de un alumno en particular
 
-// tarea profesor ... 
+// tarea profesor ...
 // GET: /upload/tasks (pasar assignment por body)  ======> devuelve todas las entregas de las materias (para el profesor)
-
-
-
-
-
-
