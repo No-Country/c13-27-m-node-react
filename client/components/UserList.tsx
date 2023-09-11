607e044e40e6af86c1e071dbcfef93a4715f8a1a
Juan Martin De Rosa <juanmderosa@gmail.com>
2023-09-11 14:10:30 -0300

    listado de alumnos por id en perfil alumno

--- client/components/UserList.tsx
+++ client/components/UserList.tsx
@@ -5,26 +5,27 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import avatar from '../public/assets/alumno-login.png';
 import styles from '../styles/materialestudio.module.scss';
+import { useParams } from 'next/navigation';
 
 const UserList = () => {
   const [userList, setUserList] = useState<UserRegister[]>([]);
+  const assignment_id = useParams();
 
   useEffect(() => {
     const getStudents = async () => {
-      const assignmentId = '64f8cd1327c1616de4f59345';
       const res = await fetch(`${mainRoute}/students/allStudents`);
 
       if (res.ok) {
         const data = await res.json();
         const studentInAssignment = data.students.filter((student: any) =>
-          student.assignments.includes(assignmentId)
+          student.assignments.includes(assignment_id.assignment)
         );
         setUserList(studentInAssignment);
       }
     };
 
     getStudents();
-  }, [userList]);
+  }, []);
 
   return (
     <section>
