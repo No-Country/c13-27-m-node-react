b6ce0c0389ed0e705cad89e7cee65995413d0139
Aron Diaz <arondiaz.cpn@gmail.com>
2023-09-11 23:24:35 -0300

    Añadí notificaciones al subir un archivo como profesor

--- client/components/SubirpdProfesor.tsx
+++ client/components/SubirpdProfesor.tsx
@@ -5,6 +5,8 @@ import styles from '../styles/subirpdf.module.scss';
 import { MdDelete, MdFileUpload } from 'react-icons/md';
 import { useAppContext } from '../context/userContext';
 import { useParams } from 'next/navigation';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const SubirpdfProfesor = () => {
   const [file, setFile] = useState<File | null>(null);
@@ -36,22 +38,17 @@ export const SubirpdfProfesor = () => {
 
   const handleUploadClick = async () => {
     if (!file) {
-      alert('Selecciona un archivo antes de subir');
+      toast.error('Selecciona un archivo antes de subir');
       return;
     }
 
     if (!id) {
-      alert('ID del estudiante no definido');
-      return;
-    }
-
-    if (Array.isArray(assignmentId)) {
-      alert('ID de asignación no válido');
+      toast.error('ID del profesor no encontrado');
       return;
     }
 
     if (!assignmentId || !assignmentId.assignment) {
-      alert('ID de la asignación no definido');
+      toast.error('ID de materia no encontrado');
       return;
     }
 
@@ -67,14 +64,13 @@ export const SubirpdfProfesor = () => {
         body: formData,
       });
       if (response.ok) {
-        alert('Archivo subido con éxito');
+        toast.success('Archivo subido con éxito!');
       } else {
         const responseData = await response.json();
-        alert(`Error: ${responseData.message || 'Error al subir el archivo'}`);
+        toast.error('Error, por favor vuelve a intentarlo');
       }
     } catch (error) {
-      console.log('Error subiendo el archivo:', error);
-      alert('Error al subir el archivo.');
+      toast.error('Error al subir archivo');
     }
   };
 
