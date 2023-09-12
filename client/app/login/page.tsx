177a2ec4a5be00fd842d1ead0886e13ac201e3e3
Aron Diaz <arondiaz.cpn@gmail.com>
2023-09-11 23:03:09 -0300

    Modifiqué el uso de las notificaciones

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,14 +1,11 @@
 import FormLogin from '../../components/FormLogin';
 import styles from '../../styles/formlogin.module.scss';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
   return (
     <main>
       <FormLogin />
       <aside className={styles.loginimagecontainer}></aside>
-      <ToastContainer />
     </main>
   );
 };
