69441a8f32a68937f5886c2c5c2a674b5b7e2e44
Matías Henríquez <matias.henriquez.dev@gmail.com>
2023-09-13 10:41:19 -0300

    Redirect arreglado

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Importo rutas de estudiantes
-const studentsRoutes = require('./studentsRoutes');
-const teachersRoutes = require('./teachersRoutes');
-const assignmentsRoutes = require('./assignmentsRoutes');
-const careersRoutes = require('./careersRoutes');
-const homePageRoutes = require('./homePageRoutes');
-const profileRoutes = require('./profileRoutes');
-const uploadsRoutes = require('./uploadsRoutes');
+const studentsRoutes = require("./studentsRoutes");
+const teachersRoutes = require("./teachersRoutes");
+const assignmentsRoutes = require("./assignmentsRoutes");
+const careersRoutes = require("./careersRoutes");
+const homePageRoutes = require("./homePageRoutes");
+const profileRoutes = require("./profileRoutes");
+const uploadsRoutes = require("./uploadsRoutes");
 
 // Middlewares
-app.use('/students', studentsRoutes);
-app.use('/teachers', teachersRoutes);
-app.use('/assignments', assignmentsRoutes);
-app.use('/careers', careersRoutes);
-app.use('/homePage', homePageRoutes);
-app.use('/profile', profileRoutes);
-app.use('/upload', uploadsRoutes);
+app.use("/students", studentsRoutes);
+app.use("/teachers", teachersRoutes);
+app.use("/assignments", assignmentsRoutes);
+app.use("/careers", careersRoutes);
+app.use("/homePage", homePageRoutes);
+app.use("/profile", profileRoutes);
+app.use("/upload", uploadsRoutes);
 
 // Middleware para redireccionar al usuario a la pagina not found 404
 app.use(async (req, res) => {
-  res.redirect('http://localhost:3000/not-found');
+  res.redirect("https://educapp-two.vercel.app/not-found");
 });
 
 module.exports = app;
